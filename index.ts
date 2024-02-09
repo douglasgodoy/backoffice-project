297ce90c6fcaa0c62adfcc38515e51803e12261b
Douglas Godoy Cardoso <douglasgodoy1998@hotmail.com>
2024-02-09 17:22:34 -0300

    feat: implement scalable system with clusters

--- index.ts
+++ index.ts
@@ -1,7 +1,26 @@
-import { startApp } from 'src/server';
+import cluster from 'cluster';
+import * as os from 'os';
 import dotenv from 'dotenv';
+import { startApp } from 'src/server';
 import dynamodb from 'src/infra/dynamo/dynamodb';
 
 dotenv.config();
 
-dynamodb.startDatabase().then(startApp);
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  const numCPUs = os.cpus().length;
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
+} else {
+  dynamodb.startDatabase().then(() => {
+    startApp();
+  });
+}
